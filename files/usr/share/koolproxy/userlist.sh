2a70b0ecb1086146db4c063e589280a3715e8432
zshwq5 <zshwq5@163.com>
2017-07-11 11:32:23 +0800

    Update userlist.sh

--- files/usr/share/koolproxy/userlist.sh
+++ files/usr/share/koolproxy/userlist.sh
@@ -8,17 +8,15 @@ if [ "Z$?" == "Z0" ]; then
 	sed -i '/youku/d' /tmp/vokins >/dev/null 2>&1
 	sed -i '/#/d' /tmp/vokins >/dev/null 2>&1
 	sed -i '$d' /tmp/vokins >/dev/null 2>&1
-	cat /tmp/vokins > /tmp/userlist.conf
-	sed -i '1,1d' /tmp/userlist.conf >/dev/null 2>&1
-	rm -f /tmp/vokins
-	if ( ! cmp -s /tmp/userlist.conf /usr/share/koolproxy/userlist.conf ); then
+	sed -i '1,1d' /tmp/vokins >/dev/null 2>&1
+	if ( ! cmp -s /tmp/vokins /usr/share/koolproxy/userlist.conf ); then
 		echo "$(date "+%F %T"): vokins 规则有更新,开始转换规则!"
-		mv /tmp/userlist.conf /usr/share/koolproxy/userlist.conf
+		mv /tmp/vokins /usr/share/koolproxy/userlist.conf
 		echo "$(date "+%F %T"): vokins 规则转换完成,应用新规则!"
 		/etc/init.d/dnsmasq restart >/dev/null 2>&1
 	else
 		echo "$(date "+%F %T"): vokins 本地规则和在线规则相同,无需更新!"
-		rm -f /tmp/userlist.conf && exit
+		rm -f /tmp/vokins && exit
 	fi
 else
 	echo "$(date "+%F %T"): 获取在线规则时出现错误!"
